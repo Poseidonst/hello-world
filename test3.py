8cbd1fb8a2dfcb727f65fa5013a70b83fd5f0153
Poseidonst <teijeonstein@gmail.com>
2018-08-23 19:08:53 +0200

    Changelaptop

--- test3.py
+++ test3.py
@@ -2,9 +2,3 @@ name = input("What's your name? ").upper()
 for i in name:
     if i not in "AEOUI":
         print(i, end = "")
-#This is from here
-<<<<<<< HEAD
-#laptop
-=======
-#pc
->>>>>>> 1731c1c75bd71b37eb6459af0004f14a0995c968
